df4107578f4892d49f86f85ea187c45acb54f40f
Miquel A. Cabot <miquel.cabot@gmail.com>
2018-03-04 23:12:52 +0100

    Searching for videos

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
+import _ from 'lodash';
 
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
@@ -17,7 +18,11 @@ class App extends Component {
       selectedVideo: null
     };
 
-    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+    this.videoSearch('surfboards');
+  }
+
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term}, (videos) => {
       this.setState({ 
         videos: videos,
         selectedVideo: videos[0]
@@ -26,9 +31,13 @@ class App extends Component {
   }
 
   render() {       // const will never change
+    const videoSearch = _.debounce((term) => { this.videoSearch(term)}, 300);
+
     return (
       <div>
-        <SearchBar />
+        <SearchBar 
+          onSearchTermChange = {videoSearch}
+        />
         <VideoDetail video={this.state.selectedVideo}/>
         <VideoList 
           videos={this.state.videos} 
